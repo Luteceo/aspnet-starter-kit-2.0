5151684632c25d2a8e42757a73ac4f9c56323f40
Tomasz Jaskula <thomasv1000@hotmail.fr>
2018-02-07 12:05:29 +0100

    Working version of kestrel with typescript and hot reloading

--- client/configureStore.ts
+++ client/configureStore.ts
@@ -10,7 +10,7 @@ export default function configureStore(history: History, initialState?: Applicat
     const windowIfDefined = typeof window === 'undefined' ? null : window as any;
     // If devTools is installed, connect to it
     const devToolsExtension = windowIfDefined && windowIfDefined.__REDUX_DEVTOOLS_EXTENSION__ as () => GenericStoreEnhancer;
-    const createStoreWithMiddleware = compose(
+    const createStoreWithMiddleware = compose<StoreEnhancerStoreCreator<any>>(
         applyMiddleware(thunk, routerMiddleware(history)),
         devToolsExtension ? devToolsExtension() : <S>(next: StoreEnhancerStoreCreator<S>) => next
     )(createStore);
